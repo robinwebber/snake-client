97a3cda17f28211339aa26353784793633071279
robinwebber <robinewebber@gmail.com>
2019-07-03 20:19:51 +0000

    added control + c to escape

--- play.js
+++ play.js
@@ -1,3 +1,21 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  const handleUserInput =
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  
+  return stdin;
+};
+
+setupInput();
